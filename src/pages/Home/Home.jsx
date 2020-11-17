d28c1a6bced360463aac964e3b35a4a0f5747c91
Mantas Razgaitis <mantas.razg@Mantass-MBP.lan>
2020-11-17 21:31:52 +0200

    Front-end date check added to Home page & Hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
-import { Section, StudentCard } from '../../components';
-import students from '../../Utils/students';
-import * as S from './Home.style';
+import React from "react";
+import { Section, StudentCard, Hero } from "../../components";
+import students from "../../Utils/students";
+import * as S from "./Home.style";
 function Home() {
+  const date = new Date();
+  let disabled = true;
+
+  if (
+    date.getHours() >= 18 &&
+    date.getHours() < 22 &&
+    date.getDay() >= 1 &&
+    date.getDay() < 5
+  ) {
+    disabled = false;
+    return (
+      <>
+        <Section>
+          <Hero date={date} disabled={disabled} />
+        </Section>
+        <Section>
+          <S.FlexDiv>
+            {students &&
+              students.map((student) => (
+                <StudentCard
+                  key={student.id}
+                  name={student.name}
+                  surname={student.surname}
+                  image={student.image}
+                  handleClick={() => window.confirm("Please confirm")}
+                />
+              ))}
+          </S.FlexDiv>
+        </Section>
+      </>
+    );
+  }
+
   return (
     <>
       <Section>
-        <S.FlexDiv>
-          {students &&
-            students.map((student) => (
-              <StudentCard
-                key={student.id}
-                name={student.name}
-                surname={student.surname}
-                image={student.image}
-                handleClick={() => window.confirm('Please confirm')}
-              />
-            ))}
-        </S.FlexDiv>
+        <Hero disabled={disabled} />
       </Section>
     </>
   );
