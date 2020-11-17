e5bc3a5a79234f3d6eaa65e8bba5613dc1c74227
Modestas Skirius <mode@Modestass-Mini.lan>
2020-11-17 20:37:02 +0200

    export Calendar Card

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export { default as Button } from './Button/Button';
-export { default as HeaderBase } from './HeaderBase/HeaderBase';
-export { default as Section } from './Section/Section';
-export { default as Loading } from './Loading/Loading';
+export { default as Button } from "./Button/Button";
+export { default as HeaderBase } from "./HeaderBase/HeaderBase";
+export { default as Section } from "./Section/Section";
+export { default as Loading } from "./Loading/Loading";
+export { default as CalendarCard } from "./CalendarCard/CalendarCard";
