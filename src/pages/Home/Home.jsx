fe950fb825b328e1c14f5d499777ca9361944d1b
MindaugasSak <m.sakenis@gmail.com>
2020-11-18 14:06:00 +0200

    env added and post timestamp on register

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Section, StudentCard, Hero, Loading } from '../../components';
 import * as S from './Home.style';
+import { useHistory } from 'react-router-dom';
+
+function sendAttendency(history, studentId) {
+  fetch(`${process.env.REACT_APP_SERVER_URL}/add-attendency`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(studentId),
+  })
+    .then(() => history.push('/view'))
+    .catch((err) => console.log(err));
+}
+
 function Home() {
   const [students, setStudents] = useState();
   const date = new Date();
   let disabled = true;
+  const history = useHistory();
 
   useEffect(() => {
-    fetch('http://localhost:8080/students')
+    fetch(`${process.env.REACT_APP_SERVER_URL}/students`)
       .then((res) => res.json())
       .then((data) => {
         setStudents(data);
@@ -15,10 +30,11 @@ function Home() {
   }, []);
 
   if (
-    date.getHours() >= 18 &&
-    date.getHours() < 22 &&
-    date.getDay() >= 1 &&
-    date.getDay() < 5
+    true
+    // date.getHours() >= 18 &&
+    // date.getHours() < 22 &&
+    // date.getDay() >= 1 &&
+    // date.getDay() < 5
   ) {
     disabled = false;
     return (
@@ -36,7 +52,11 @@ function Home() {
                   surname={student.surname}
                   image={student.image}
                   email={student.email}
-                  handleClick={() => window.confirm('Please confirm')}
+                  handleClick={() =>
+                    window.confirm('Please confirm')
+                      ? sendAttendency(history, student.id)
+                      : console.log('Canceled')
+                  }
                 />
               ))
             ) : (
@@ -46,29 +66,14 @@ function Home() {
         </Section>
       </>
     );
-  }
-
-  return (
-    <>
-      <Section>
-        <S.FlexDiv>
-          {students &&
-            students.map((student) => (
-              <StudentCard
-                key={student.id}
-                name={student.name}
-                surname={student.surname}
-                image={student.image}
-                email={student.email}
-                handleClick={() => window.confirm('Please confirm')}
-              />
-            ))}
-        </S.FlexDiv>
-
-        <Hero disabled={disabled} />
-      </Section>
-    </>
-  );
+  } else
+    return (
+      <>
+        <Section>
+          <Hero disabled={disabled} />
+        </Section>
+      </>
+    );
 }
 
 export default Home;
