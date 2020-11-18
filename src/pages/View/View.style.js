85b0fe128c479378c6c373f5ea0ae8896d3c0a3d
Modestas Skirius <mode@Modestass-Mini.lan>
2020-11-18 13:39:08 +0200

    add selectBlock to view page

---
+++ src/pages/View/View.style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const InputCont = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+export const InputWrapp = styled.div`
+  width: 100%;
+  max-width: 300px;
+  margin: 10px;
+`;
+
+export const InputLabel = styled.label`
+  font-weight: bold;
+`;
