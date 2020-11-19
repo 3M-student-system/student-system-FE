3ac949ccc70416dd91706221066aae3aeb7d0aec
Modestas Skirius <mode@Modestass-Mini.lan>
2020-11-19 16:21:58 +0200

    add studentListCard to action

--- src/components/CalendarTable/CalendarTable.jsx
+++ src/components/CalendarTable/CalendarTable.jsx
@@ -1,12 +1,23 @@
-import React from "react";
-import { CalendarCard } from "../../components";
+import React, { useState } from "react";
+import { CalendarCard, StudentListCard } from "../../components";
 import * as S from "./CalendarTable.style";
 
 function CalendarTable({ days, month }) {
+  const [clickedDay, setClickedDay] = useState();
+  const [onOff, setOnOff] = useState(false);
+
   return (
     <S.TableCont>
       <S.Month>{month ? month.name : "All"}</S.Month>
-
+      {clickedDay && (
+        <S.ListCardCont>
+          <StudentListCard
+            dataofDay={clickedDay}
+            listVisible={onOff}
+            CloseList={() => setOnOff(false)}
+          />
+        </S.ListCardCont>
+      )}
       {(days || []).map((day) => (
         <CalendarCard
           key={day.date}
@@ -14,6 +25,11 @@ function CalendarTable({ days, month }) {
           month={day.date.split("-")[1]}
           year={day.date.split("-")[0]}
           dayData={day}
+          handleClick={() => {
+            setClickedDay(day);
+            setOnOff(!onOff);
+            //console.log(day);
+          }}
         />
       ))}
     </S.TableCont>
---
+++ src/components/StudentListCard/StudentListCard.style.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { Button } from "../../components";
+
+export const ListWrapp = styled.div`
+  width: 100%;
+  display: ${(prop) => (prop.listVisible === true ? "block" : "none")};
+  flex-direction: column;
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-sizing: border-box;
+  box-shadow: 5px 5px 15px 5px #919191;
+`;
+export const Head = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+  border-bottom: 1px solid black;
+  padding: 10px 0;
+  justify-content: space-between;
+`;
+export const H3 = styled.h3`
+  margin: 5px 0;
+`;
+export const H4 = styled.h4`
+  margin: 3px;
+`;
+export const StyledButton = styled(Button)`
+  margin: 10px;
+`;
+export const Line = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
